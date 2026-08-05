8e006bc24841ee5c9b185e1f2de30b9f7a4f00c6
Valeria <valeria.cruz.galindo03@gmail.com>
2026-08-05 13:21:24 -0600

    regresar a base vectorial de Chroma

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -11,6 +11,10 @@ https://docs.djangoproject.com/en/6.0/ref/settings/
 """
 
 from pathlib import Path
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/6.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-he#f6y#82mpo_#*o)xotniu1vlfot09lxc8pztemka3*q!2lat'
+SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -38,6 +42,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'corsheaders',
+    
 ]
 
 MIDDLEWARE = [
